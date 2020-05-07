a31f108d58cc77f516b509ead987a7a342196b99
Florent Lambrigger <flambrigger@protonmail.com>
2020-05-07 17:04:49 +0200

    add socket.io

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-server.listen(port,()=>{
+server.listen(port, () => {
   console.log(`listen on port ${port} `)
 })
 
