d715daae2eb41d993b2d6beca4cdae0a24a1469f
Florent Lambrigger <flambrigger@protonmail.com>
2020-05-08 01:43:34 +0200

    add url timestamp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
-const { generateMessage } = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
   })
   socket.on('sendLocation', (position, callback) => {
 
-    io.emit('locationMessage', `https://google.com/maps/?q=${position.lat},${position.lon}`)
+    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps/?q=${position.lat},${position.lon}`))
     callback()
   })
 
